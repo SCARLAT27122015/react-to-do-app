371a859287553df4b59cfc7ef37b085a8042256f
SCARLAT27122015 <mansion.lagrimas@gmail.com>
2020-10-03 11:25:54 -0500

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Button, FormControl, FormHelperText, Input, InputLabel} from '@material-ui/core';
+
 
 function App() {
+  const [Todos, setTodos] = useState(['First task', 'Second task']);
+  const [Entry, setEntry] = useState('');
+  
+  const addTodo = () => {
+    setTodos([...Todos, Entry]);
+    setEntry('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To do App</h1>
+      <form>
+      
+        <FormControl>
+          <InputLabel>Enter a To-do</InputLabel>
+          <Input value={ Entry } onChange={ e => setEntry(e.target.value)}/>
+        </FormControl>
+        
+        <Button type="submit" disabled={!Entry} onClick={ addTodo } variant="contained" color="primary">
+          Add todo
+        </Button>
+      </form>
+      
+      <ul>
+        {Todos.map((todo,i) => 
+          <li key={i}>{ todo }</li>)}
+      </ul>
     </div>
   );
 }
